5c6c63fc4f01a5b680ab7dd0b1b44c465ff8de5f
hasnath.jami.chowdhury <hasnath.jami.chowdhury@gmail.com>
2024-03-23 23:33:08 +0600

    Deep dive into Text and Container

--- lib/main.dart
+++ lib/main.dart
@@ -18,14 +18,38 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Home"),
-      ),
-      body:  Container(
-        child: Center(
-          child: Text("Jami"),
-        )
-      )
-    );
+        appBar: AppBar(
+          title: const Text("Home"),
+        ),
+        body: Center(
+          child: Container(
+              // padding: const EdgeInsets.all(8),
+              alignment: Alignment.center,
+              //   color: Colors.teal,
+              height: 100,
+              width: 100,
+              decoration: BoxDecoration(
+                  //color: Colors.teal,
+                  //shape: BoxShape.circle,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey.shade400,
+                        blurRadius: 5,
+                        // spreadRadius: 10,
+                        offset: Offset(2.0, 5.0))
+                  ],
+                  gradient:
+                      LinearGradient(colors: [Colors.yellow, Colors.pink])),
+              child: Text(
+                "I am a Big Box eeee",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              )),
+        ));
   }
 }
